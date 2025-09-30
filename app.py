be7f0f0cbc62c27663ef9c5835fe7b81b53427d9
cadee-exe <cadeez@nycstudents.net>
2025-09-30 06:24:16 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+count = 1
+
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1
+
+""" number = 1
+while number > 0:
+    print("This will never stop!") """
+
+""" order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!") """
+
+count = 1
+while count   <= 10:
+    print("This is loop number", count)
+    count = count + 1
+        
+""" color = """
+
+while color != "stop":
+      color = input("What is your favorite color? (type 'stop' to finish): ")
+                    
+print("Thanks for answering!")
+
+
+
+
+
+
+
+
+
+
